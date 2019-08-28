fd83e3719ebf94564e13394b1ed5f52ac06d6e3e
Monika-Mistry <Monika-Mistry@users.noreply.github.com>
2019-08-28 10:20:39 +0100

    Update get_docker.sh

--- get_docker.sh
+++ get_docker.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-user=$(whoami)
-
 curl https://get.docker.com | sudo bash
-sudo usermod -aG docker $user
+sudo usermod -aG docker $USER
 sudo systemctl docker start
 sudo systemctl docker enable
+
+
 
 echo "Please restart your terminal"
